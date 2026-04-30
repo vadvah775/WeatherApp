b58b474df294baa5bc4167143c700cf5bf2c59af
riim <vadvah775@mail.ru>
2026-04-30 13:48:11 +0300

    add auth test

--- src/test/java/com/weatherApp/BaseIntegrationTest.java
+++ src/test/java/com/weatherApp/BaseIntegrationTest.java
@@ -1,4 +1,13 @@
 package com.weatherApp;
 
-public class BaseIntegrationTest {
+import com.weatherApp.configs.TestDatabaseConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {TestDatabaseConfig.class})
+@Transactional
+public abstract class BaseIntegrationTest {
 }
