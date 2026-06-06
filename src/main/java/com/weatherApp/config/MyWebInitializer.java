08dc782dd2586b8dcf8d76cf1febfefd073e999a
riim <vadvah775@mail.ru>
2026-06-06 22:57:37 +0300

    session tracking mode via cookies is set

--- src/main/java/com/weatherApp/config/MyWebInitializer.java
+++ src/main/java/com/weatherApp/config/MyWebInitializer.java
@@ -1,11 +1,23 @@
 package com.weatherApp.config;
 
 import jakarta.servlet.Filter;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.SessionTrackingMode;
 import org.jspecify.annotations.Nullable;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import java.util.Set;
+
 public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.setSessionTrackingModes(Set.of(SessionTrackingMode.COOKIE));
+        super.onStartup(servletContext);
+    }
+
     @Override
     protected Class<?> @Nullable [] getRootConfigClasses() {
         return new Class[] {DatabaseConfig.class, AppConfig.class}; // корневой контекст (сервисы, DAO)
